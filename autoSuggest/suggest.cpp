2dbaee552cf65c00bd6d97bfd0a9bdf325d18135
Newton Shahi Thakuri <114736550+GrimNick@users.noreply.github.com>
2025-05-25 20:23:42 +0545

    working n gram
    will make n grams from 2 to definitions.

--- autoSuggest/suggest.cpp
+++ autoSuggest/suggest.cpp
@@ -47,8 +47,20 @@ vector<string> sampleWords(){
     return sampledWords;
 }
 
+
 map<string, vector<string>> getn_grams(vector<string> words){
     map<string, vector<string>> n_grams;
+    for(int j=0;j<words.size();j++){
+        vector<string> brokeDownString;
+        int k =2;
+        for(int k=2;k<=words[j].length();k++){
+        for(int i=0;i +k <= words[j].length();i++){
+            
+                brokeDownString.push_back(words[j].substr(i,k));
+            }   
+        }
+        n_grams.insert({words[j],brokeDownString});
+    }
 
     return n_grams;
 }
@@ -59,7 +71,18 @@ int main(){
     for(int i=0;i<words.size(); i++) cout<<words[i]<<",";
     cout<<"\n\n**** End of samppled words *****\n\n";
     map<string, vector<string>> ngram = getn_grams(words);
+    cout<<"\n******** n-gram of words are : *******\n" <<endl;
 
+    for(int i=0;i<2;i++){
+        vector<string> toDisplayString = ngram[words[i]];
+        cout<<words[i]<<"\t";
+        for(int j=0;j<toDisplayString.size();j++){
+            cout<< toDisplayString[j]<<" | ";
+        }
+        cout<<"\n";
+    }
+
+    cout<<"\n\n**** End of n-gram of words *****\n\n";
 
     return 0;
 }
