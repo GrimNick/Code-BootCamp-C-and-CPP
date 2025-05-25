d0514144005ae80f4848e61afcd92b7a6be911e0
Newton Shahi Thakuri <114736550+GrimNick@users.noreply.github.com>
2025-05-25 22:07:13 +0545

    completed auto Suggestion

--- autoSuggest/suggest.cpp
+++ autoSuggest/suggest.cpp
@@ -49,18 +49,22 @@ vector<string> sampleWords(){
     return sampledWords;
 }
 
+vector<string> getSingleGrams(string word){
+        vector<string> brokeDownString;
+        for(int k=2;k<=word.length();k++){
+        for(int i=0;i +k <= word.length();i++){
+            
+                brokeDownString.push_back(word.substr(i,k));
+            }   
+        }
+        return brokeDownString;
+}
+
 //gives n gram from 2 to definition
 map<string, vector<string>> getn_grams(vector<string> words){
     map<string, vector<string>> n_grams;
     for(int j=0;j<words.size();j++){
-        vector<string> brokeDownString;
-        int k =2;
-        for(int k=2;k<=words[j].length();k++){
-        for(int i=0;i +k <= words[j].length();i++){
-            
-                brokeDownString.push_back(words[j].substr(i,k));
-            }   
-        }
+        vector<string> brokeDownString = getSingleGrams(words[j]);
         n_grams.insert({words[j],brokeDownString});
     }
 
@@ -91,6 +95,48 @@ map<string, int> getFregMap(vector<string> words){
     }
     return FregMap;
 }
+
+map<string,int> buildNGramIndex(const map<string, vector<string>>& ngrams){
+    map<string,int> ngramIndex;
+    int idx=0;
+    for(const auto p:ngrams){
+        for(const string ng:p.second){
+            if(ngramIndex.find(ng) == ngramIndex.end()){
+                ngramIndex[ng] = idx ++;
+            }
+        }
+    }
+    return ngramIndex;
+}
+
+vector<double> vectorizeWord(const vector<string> wordNgrams, const map<string, int>& ngramIndex){
+    vector<double> vec(ngramIndex.size(),0.0);
+    for(const string ng:wordNgrams){
+        auto it = ngramIndex.find(ng);
+        if(it!=ngramIndex.end()){
+            vec[it->second] += 1.0;
+        }
+    }
+    return vec;
+} 
+
+double cosineSimilarity(const vector<double> A,const vector<double> B){
+    if(A.size() != B.size()){
+        cerr<< "Vectors must be the same size"<<endl;
+        return 0.0;
+    }
+
+    double dot =0.0,normA =0.0,normB=0.0;
+    for( size_t i=0;i<A.size();i++){
+        dot+=A[i] * B[i];
+        normA += A[i]*A[i];
+        normB += B[i]*B[i];
+    }
+    if(normA ==0 || normB ==0) return 0.0;
+    return dot/(sqrt(normA)* sqrt(normB));
+
+}
+
 
 int main(){
     vector<string> words = sampleWords();
@@ -147,6 +193,42 @@ int main(){
     } 
     cout<<"\n\n**** End frequency of n gram words *****\n\n";
 
+
+    map<string,int> ngramIndex = buildNGramIndex(ngram);
+
+    cout<<"\n ------total unique n-gram:"<<ngramIndex.size()<<"\n\n";
+
+    vector<vector<double>> dictVectors;
+    for(string w:words){
+        dictVectors.push_back(vectorizeWord(ngram[w],ngramIndex));
+    }
+
+    //input
+    string inputWord;
+    cout<<"~~~~~ Enter a word to compare and get auto suggest ~~~"<<"\n\n => ";
+    cin>> inputWord;
+
+    vector<string> inputNGrams = getSingleGrams(inputWord);
+
+    //vectorize
+    vector<double> inputVec = vectorizeWord(inputNGrams,ngramIndex);
+    
+    //compute cosine against all dict words
+    vector<pair<double,int>> similarityScores;
+    for(int i=0;i<dictVectors.size();i++){
+        double score = cosineSimilarity(inputVec, dictVectors[i]);
+        similarityScores.push_back({score,i});
+    }
+
+    //sort descending
+    sort(similarityScores.begin(), similarityScores.end(), [](auto& a, auto& b){
+        return a.first > b.first;
+    });
+
+    cout<<"\n top 10 matches :\n";
+    for(int i=0; i<10 && i<similarityScores.size(); i++){
+        cout<< words[similarityScores[i].second] << " ( Score : " <<similarityScores[i].first <<")\n";
+    }
 
     return 0;
 }
